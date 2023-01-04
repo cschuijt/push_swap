4b82e5ed2e057c52c758bf1e2617b57e95da0f5d
Casper Schuijt <cschuijt@student.codam.nl>
2023-01-04 15:03:11 +0100

    Prepare to sort

--- sorting_algorithm_selection.c
+++ sorting_algorithm_selection.c
@@ -1,32 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   sorting_algorithm_selection.c                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/01/02 23:27:32 by cschuijt      #+#    #+#                 */
-/*   Updated: 2023/01/02 23:27:32 by cschuijt      ########   odam.nl         */
+/*   Created: 2023/01/04 14:26:53 by cschuijt      #+#    #+#                 */
+/*   Updated: 2023/01/04 14:26:53 by cschuijt      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+void	sort_stack(t_push_swap *push_swap)
 {
-	t_push_swap	*push_swap;
-
-	if (argc == 1)
-		return (1);
-	else if (argc == 2)
-		return (0);
-	push_swap = ft_calloc(sizeof(t_push_swap), 1);
-	if (!push_swap)
-		exit_perror("malloc error");
-	initialize_stack(push_swap, argv, argc);
-	print_stacks(push_swap);
-	// sort_stack(push_swap);
-	// print_stacks(push_swap);
-	// print_instructions(push_swap);
-	return (0);
 }
